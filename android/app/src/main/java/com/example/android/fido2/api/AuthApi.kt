7a7ccfb59cb661e01e9bdd2bfd9757075c9ce129
Anas El Hajjaji <anas.elhajjaji@ticketmaster.com>
2022-09-19 14:18:21 -0400

    extracts the attestation

--- android/app/src/main/java/com/example/android/fido2/api/AuthApi.kt
+++ android/app/src/main/java/com/example/android/fido2/api/AuthApi.kt
@@ -24,6 +24,7 @@ import com.example.android.fido2.BuildConfig
 import com.example.android.fido2.decodeBase64
 import com.example.android.fido2.toBase64
 import com.google.android.gms.fido.fido2.api.common.Attachment
+import com.google.android.gms.fido.fido2.api.common.AttestationConveyancePreference
 import com.google.android.gms.fido.fido2.api.common.AuthenticatorAssertionResponse
 import com.google.android.gms.fido.fido2.api.common.AuthenticatorAttestationResponse
 import com.google.android.gms.fido.fido2.api.common.AuthenticatorSelectionCriteria
@@ -127,7 +128,7 @@ class AuthApi @Inject constructor(
                 .url("$BASE_URL/registerRequest")
                 .addHeader("Cookie", formatCookie(sessionId))
                 .method("POST", jsonRequestBody {
-                    name("attestation").value("none")
+                    name("attestation").value("direct")
                     name("authenticatorSelection").objectValue {
                         name("authenticatorAttachment").value("platform")
                         name("userVerification").value("required")
@@ -309,7 +310,9 @@ class AuthApi @Inject constructor(
                     "challenge" -> builder.setChallenge(reader.nextString().decodeBase64())
                     "pubKeyCredParams" -> builder.setParameters(parseParameters(reader))
                     "timeout" -> builder.setTimeoutSeconds(reader.nextDouble())
-                    "attestation" -> reader.skipValue() // Unused
+                    "attestation" -> builder.setAttestationConveyancePreference(
+                        AttestationConveyancePreference.fromString(reader.nextString())
+                    )
                     "excludeCredentials" -> builder.setExcludeList(
                         parseCredentialDescriptors(reader)
                     )
