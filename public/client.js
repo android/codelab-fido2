810c7169e23fb2ed9369b5d16ad21e53dd14681e
agektmr <agektmr@gmail.com>
2025-04-13 09:11:40 +0000

    Remove CDN reference

--- public/client.js
+++ public/client.js
@@ -14,6 +14,27 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
+
+/**
+ * Encode given buffer or decode given string with Base64URL.
+ */
+export class base64url {
+  static encode(buffer) {
+    const base64 = window.btoa(String.fromCharCode(...new Uint8Array(buffer)));
+    return base64.replace(/=/g, '').replace(/\+/g, '-').replace(/\//g, '_');
+  }
+
+  static decode(base64url) {
+    const base64 = base64url.replace(/-/g, '+').replace(/_/g, '/');
+    const binStr = window.atob(base64);
+    const bin = new Uint8Array(binStr.length);
+    for (let i = 0; i < binStr.length; i++) {
+      bin[i] = binStr.charCodeAt(i);
+    }
+    return bin.buffer;
+  }
+}
+
 export const _fetch = async (path, payload = '') => {
   const headers = {
     'X-Requested-With': 'XMLHttpRequest',
